5a032f31d9ce3d4efd210f908e567e37bd9a37ec
AryanTomar0001 <151915150+AryanTomar0001@users.noreply.github.com>
2026-03-12 11:50:57 +0530

    Update LeetCode_1760.java

--- LeetCode_1760.java
+++ LeetCode_1760.java
@@ -1,15 +1,50 @@
 class Solution {
-    public int minimumSize(int[] A, int k) {
-        int left = 1, right = 1_000_000_000;
-        while (left < right) {
-            int mid = (left + right) / 2, count = 0;
-            for (int a : A)
-                count += (a - 1) / mid;
-            if (count > k)
-                left = mid + 1;
-            else
-                right = mid;
+    private boolean withinRange(int[] nums,int maxOperations,int k)
+    {
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+           if(nums[i]>k)
+           {
+            maxOperations-=(nums[i]-1)/k;
+           }
+           if(maxOperations<0)
+           {
+            return false;
+           } 
         }
-        return left;
+        return true;
+    }
+    public int minimumSize(int[] nums, int maxOperations) {
+        //i - th bag : nums[i] balls
+        //at most maxOperations times
+        //any bag of balls : 2 bags with pos no of balls
+        //penalty : max no of balls in a bag
+        //minimize max no of balls
+
+        // 9 : 9/(maxOperations + 1)
+        
+        int max=nums[0];
+        int n=nums.length;
+
+        for(int i=0;i<n;i++)
+        {
+            if(max<nums[i])
+              max=nums[i];
+        }
+        int left=1,right=max;
+        while(left<=right)
+        {
+           int mid=left+(right-left)/2;
+           if(withinRange(nums,maxOperations,mid))
+           {
+            right=mid-1;
+           } 
+           else
+           {
+            left=mid+1;
+           }
+        }
+    return left;
     }
 }
